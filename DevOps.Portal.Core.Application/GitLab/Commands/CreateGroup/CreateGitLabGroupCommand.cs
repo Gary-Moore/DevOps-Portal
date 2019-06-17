6c7295e9c43eda94dd1fc3b08dc06e41a025bc62
Gary Moore <garyprm@gmail.com>
2019-06-17 21:52:59 +0100

    Navigation Header (#15)
    * Fix compllation errors
    
    * Remove .net projects
    
    * Add new nav component
    
    * Add nav and side nav components

--- DevOps.Portal.Core.Application/GitLab/Commands/CreateGroup/CreateGitLabGroupCommand.cs
+++ DevOps.Portal.Core.Application/GitLab/Commands/CreateGroup/CreateGitLabGroupCommand.cs
@@ -1,11 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using DevOps.Portal.Core.Application.UltraBuild;
 using DevOps.Portal.Core.Application.UltraBuild.Commands;
 
 namespace DevOps.Portal.Core.Application.GitLab.Commands.CreateGroup
 {
     public class CreateGitLabGroupCommand : IUltraBuildCommand
     {
+        public Task ExecuteAsync(UltraBuildModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UndoAsync(UltraBuildModel model)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
