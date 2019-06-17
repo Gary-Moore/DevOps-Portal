7e5d9c84965c79b680493cac77211e6b01d9da82
Gary Moore <garyprm@outlook.com>
2019-06-17 21:47:13 +0100

    Add nav and side nav components

--- DevOps.Portal.Client/ClientApp/src/app/app.module.ts
+++ DevOps.Portal.Client/ClientApp/src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './core/nav/nav.component';
+import { SideNavComponent } from './core/side-nav/side-nav.component';
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faTachometerAlt, faHammer, faCogs,
+  faCheck, faTimesCircle, faStickyNote, faExclamationCircle, faInfoCircle,
+  faExclamationTriangle, faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faTachometerAlt, faHammer, faCogs, faCheck, faTimesCircle,
+  faStickyNote, faExclamationCircle, faInfoCircle, faExclamationTriangle, faCalendarAlt);
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    SideNavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
