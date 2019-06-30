812d136813c08ffb3c0545cd0ea70b5ff0153668
Gary Moore <garyprm@outlook.com>
2019-06-30 20:02:53 +0100

    add service plan to terraform file

--- Terraform/clientwebapp.tf
+++ Terraform/clientwebapp.tf
@@ -11,13 +11,30 @@ provider "azurerm" {
   
 }
 
-
 resource "azurerm_resource_group" "devops_portal" {
   name = "gw-devops-portal-rg"
   location = "West Europe"
 
   tags = {
       product = "devops portal",
+      organisation = "GammaWeb Solutions"
+  }
+}
+
+resource "azurerm_app_service_plan" "devops_portal" {
+  name = "gw-devops-portal-serviceplan"
+  location = "${azurerm_resource_group.devops_portal.location}"
+  resource_group_name = "${azurerm_resource_group.devops_portal.name}"
+  kind = "Linux"
+  reserved = true
+
+  sku = {
+    tier = "Free",
+    size = "F1"
+  }
+
+  tags = {
+    product = "devops portal",
       organisation = "GammaWeb Solutions"
   }
 }
