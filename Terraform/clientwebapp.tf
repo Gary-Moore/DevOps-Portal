8b95ad8dd995b3f401c7ddd8d7dc8e8ed78e15c8
Gary Moore <garyprm@outlook.com>
2019-06-30 20:37:35 +0100

    Add app service to terraform

--- Terraform/clientwebapp.tf
+++ Terraform/clientwebapp.tf
@@ -7,6 +7,14 @@ terraform {
   }
 }
 
+variable "product" {
+  default = "devops portal"
+}
+
+variable "organisation" {
+  default = "GammaWeb Solutions"
+}
+
 provider "azurerm" {
   
 }
@@ -16,8 +24,8 @@ resource "azurerm_resource_group" "devops_portal" {
   location = "West Europe"
 
   tags = {
-      product = "devops portal",
-      organisation = "GammaWeb Solutions"
+      product = "${var.product}",
+      organisation = "${var.organisation}"
   }
 }
 
@@ -34,7 +42,21 @@ resource "azurerm_app_service_plan" "devops_portal" {
   }
 
   tags = {
-      product = "devops portal",
-      organisation = "GammaWeb Solutions"
+      product = "${var.product}",
+      organisation = "${var.organisation}"
   }
 }
+
+resource "azurerm_app_service" "devops_portal" {
+  name = "gw-devops-portal-client"
+  location = "${azurerm_resource_group.devops_portal.location}"
+  resource_group_name = "${azurerm_resource_group.devops_portal.name}"
+  app_service_plan_id = "${azurerm_app_service_plan.devops_portal.id}"
+  https_only = true
+
+  tags = {
+      product = "${var.product}",
+      organisation = "${var.organisation}"
+  }
+}
+
